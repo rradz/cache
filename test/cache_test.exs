6e7b267962082252a662e4ba406b59d12b034bd6
Rafal Radziszewski <rafal.radziszewski@gmail.com>
2022-10-08 23:45:16 +0200

    Added some basic tests and structure for more

--- test/cache_test.exs
+++ test/cache_test.exs
@@ -2,7 +2,63 @@ defmodule CacheTest do
   use ExUnit.Case
   doctest Cache
 
-  test "greets the world" do
-    assert Cache.hello() == :world
+  defmodule DummyFunctions do
+    def success, do: {:ok, 42}
+
+    def success_with_delay do
+      Process.sleep(1_000)
+
+      {:ok, 42}
+    end
+
+    def failure, do: {:error, "Universe is malfunctioning"}
+  end
+
+  describe "registration" do
+    test "correctly registers the function" do
+      assert :ok = Cache.register_function(&DummyFunctions.success/0, "success", 1_000, 10_000)
+      assert :ok = Cache.register_function(&DummyFunctions.success/0, "success_2", 1_000, 10_000)
+      assert :ok = Cache.register_function(&DummyFunctions.failure/0, "failure", 1_000, 10_000)
+    end
+
+    test "doesn't allow registering under the same key twice" do
+      assert :ok = Cache.register_function(&DummyFunctions.success/0, "success", 1_000, 10_000)
+
+      assert {:error, :already_registered} =
+               Cache.register_function(&DummyFunctions.success/0, "success", 1_000, 10_000)
+    end
+  end
+
+  describe "retrieval" do
+    setup do
+      :ok = Cache.register_function(&DummyFunctions.success/0, "success", 1_000, 10_000)
+
+      :ok =
+        Cache.register_function(
+          &DummyFunctions.success_with_delay/0,
+          "success_with_delay",
+          1_000,
+          10_000
+        )
+
+      :ok = Cache.register_function(&DummyFunctions.failure/0, "failure", 1_000, 10_000)
+    end
+
+    test "returns the value of the registered function" do
+      assert {:ok, 42} = Cache.get("success")
+    end
+
+    test "returns error when function is not registered" do
+      assert {:error, :not_registered} = Cache.get("unregistered")
+    end
+
+    test "gives the last value when function is recomputed" do
+    end
+
+    test "waits for timeout when result unavailable" do
+    end
+
+    test "results in error if timeout is reached" do
+    end
   end
 end
