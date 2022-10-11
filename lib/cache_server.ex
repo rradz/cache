6e2d9d5a157c31496bd326ea88f36123ead9cbe5
Rafal Radziszewski <rafal.radziszewski@gmail.com>
2022-10-11 10:10:06 +0200

    Implementation mostly done, testing and more docs pending

--- lib/cache_server.ex
+++ lib/cache_server.ex
@@ -14,21 +14,34 @@ defmodule Cache.Server do
     {:ok, %{store: pid, function_map: %{}}}
   end
 
-  def handle_call({:register_function, {key, fun, ttl, refresh_interval}}, _from, state) do
-    case Map.get(fun_map, key) do
+  def handle_call(
+        {:register_function, {key, fun, ttl, refresh_interval}},
+        _from,
+        %{function_map: function_map} = state
+      ) do
+    case Map.get(function_map, key) do
       nil ->
-        new_state =
-          Map.put(fun_map, key, %{function: fun, ttl: ttl, refresh_interval: refresh_interval})
+        new_function_map =
+          Map.put(function_map, key, %{
+            function: fun,
+            ttl: ttl,
+            refresh_interval: refresh_interval
+          })
 
-        {:reply, :ok, new_state}
+        send(self(), {:recalculate_function, {key, ttl, refresh_interval}})
+
+        {:reply, :ok, %{state | function_map: new_function_map}}
 
       _ ->
         {:reply, {:error, :already_registered}, state}
     end
   end
 
-  def handle_info({:recalculate_function, {key, ttl, refresh_interval}}, state) do
-    %{function: fun} = Map.get(state, key)
+  def handle_info(
+        {:recalculate_function, {key, ttl, refresh_interval}},
+        %{function_map: function_map} = state
+      ) do
+    %{function: fun} = Map.get(function_map, key)
     server_pid = self()
 
     Process.spawn(
@@ -43,8 +56,47 @@ defmodule Cache.Server do
       end,
       [:link]
     )
+
+    Process.send_after(
+      server_pid,
+      {:recalculate_function, {key, ttl, refresh_interval}},
+      refresh_interval
+    )
+
+    {:noreply, state}
   end
 
-  def handle_info({:update_function_value, {key, value, ttl}}, state) do
+  def handle_info({:update_function_value, {key, value, ttl}}, %{store: store} = state) do
+    Cache.Store.store(store, key, value, ttl)
+
+    {:noreply, state}
+  end
+
+  def handle_call(
+        {:get, {key, timeout, _opts}},
+        from,
+        %{store: store, function_map: function_map} = state
+      ) do
+    with %{} <- Map.get(function_map, key, :not_registered),
+         {:ok, value} <- Cache.Store.get(store, key) do
+      {:reply, {:ok, value}, state}
+    else
+      :not_registered ->
+        {:reply, {:error, :not_registered}, state}
+
+      :no_value ->
+        Process.send_after(self(), {:delayed_get, {key, from}}, timeout)
+        {:noreply, state}
+    end
+  end
+
+  def handle_info({:delayed_get, {key, from}}, %{store: store} = state) do
+    case Cache.Store.get(store, key) do
+      {:ok, value} ->
+        send(from, {:ok, value})
+
+      _ ->
+        send(from, {:error, :timeout})
+    end
   end
 end
